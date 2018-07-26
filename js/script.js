79045645739bdb04b0bef1870518cb76120b3f5f
Ilya <Ilya@ggmail.com>
2018-07-26 23:24:32 +0300

    Это наш комит

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+var LeftBorderWidth = 1;
+
+const pi = 3.14;
+
+{
+	let second = 2;
+}
+
+
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+
+
+
+console.log("10" == 10);
